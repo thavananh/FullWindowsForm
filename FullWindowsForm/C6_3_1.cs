b6df4d8a09577a96386019d880dc57e663c3a9fd
DuySieuDepTrai <4801104035@student.hcmue.edu.vn>
2023-11-18 13:25:31 +0700

    123123

--- FullWindowsForm/C6_3_1.cs
+++ FullWindowsForm/C6_3_1.cs
@@ -27,6 +27,17 @@ namespace FullWindowsForm
             }
         }
 
+        private void addItemVaoLopAVaB()
+        {
+            lstbLopA.Items.Add("Trần Hùng");
+            lstbLopA.Items.Add("Nguyễn Hưng");
+            lstbLopA.Items.Add("Lâm Tứ");
+            lstbLopB.Items.Add("Tuấn Hưng");
+            lstbLopB.Items.Add("Hồng Ngọc");
+            lstbLopB.Items.Add("Anh Minh");
+            lstbLopB.Items.Add("Bảo Lâm");
+        }
+
         private void endMenuStrip_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -44,17 +55,45 @@ namespace FullWindowsForm
 
         private void mnst_ChuyenSangLopB_Click(object sender, EventArgs e)
         {
-
+            for (int i = lstbLopB.Items.Count - 1; i >= 0; i--)
+            {
+                if (lstbLopB.GetSelected(i))
+                {
+                    lstbLopA.Items.Add(lstbLopB.Items[i]);
+                    lstbLopB.Items.RemoveAt(i);
+                }
+            }
         }
 
         private void mnst_ChuyenSangLopA_Click(object sender, EventArgs e)
         {
-
+            for (int i = lstbLopA.Items.Count - 1; i >= 0; i--)
+            {
+                if (lstbLopA.GetSelected(i))
+                {
+                    lstbLopB.Items.Add(lstbLopA.Items[i]);
+                    lstbLopA.Items.RemoveAt(i);
+                }
+            }
         }
 
         private void mnst_XoaHocVien_Click(object sender, EventArgs e)
         {
+            for (int i = lstbLopA.Items.Count - 1; i >= 0; i--)
+            {
+                if (lstbLopA.GetSelected(i))
+                {
+                    lstbLopA.Items.RemoveAt(i);
+                }
+            }
 
+            for (int i = lstbLopB.Items.Count - 1; i >= 0; i--)
+            {
+                if (lstbLopB.GetSelected(i))
+                {
+                    lstbLopB.Items.RemoveAt(i);
+                }
+            }
         }
     }
 }
